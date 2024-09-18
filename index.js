32f032b67d3a3a85e4a6333ad7fbe69c6fcc2bdc
suman7490 <suman.dhangar.135497@gmail.com>
2024-09-18 13:55:11 +0530

    normal changes

--- index.js
+++ index.js
@@ -1,50 +1,25 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+      res.send('Hello World!')
 })
-const data = {
-    "login": "hiteshchaudhary",
-    "id": 17576078,
-    "node_id": "MDQ6VXNlcjE3NTc2MDc4",
-    "avatar_url": "https://avatars.githubusercontent.com/u/17576078?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/hiteshchaudhary",
-    "html_url": "https://github.com/hiteshchaudhary",
-    "followers_url": "https://api.github.com/users/hiteshchaudhary/followers",
-    "following_url": "https://api.github.com/users/hiteshchaudhary/following{/other_user}",
-    "gists_url": "https://api.github.com/users/hiteshchaudhary/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/hiteshchaudhary/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/hiteshchaudhary/subscriptions",
-    "organizations_url": "https://api.github.com/users/hiteshchaudhary/orgs",
-    "repos_url": "https://api.github.com/users/hiteshchaudhary/repos",
-    "events_url": "https://api.github.com/users/hiteshchaudhary/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/hiteshchaudhary/received_events",
-    "type": "User",
-    "site_admin": false,
-    "name": null,
-    "company": null,
-    "blog": "",
-    "location": null,
-    "email": null,
-    "hireable": null,
-    "bio": null,
-    "twitter_username": null,
-    "public_repos": 0,
-    "public_gists": 0,
-    "followers": 5,
-    "following": 0,
-    "created_at": "2016-03-01T18:11:15Z",
-    "updated_at": "2016-03-01T18:11:15Z"
-  }
 app.get('/home', (req, res) => {
-    res.send('<h1>Hello Suman!</h1>')
-})
-app.get('/suman', (req, res) => {
-    res.json(data)
+  res.send('Hello Suman!')
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`app is listening`)
+
+
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`app is listening`)
 })
